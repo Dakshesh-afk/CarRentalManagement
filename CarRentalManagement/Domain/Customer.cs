a882b0843688300e71c53025c2ca2a901a32bbe6
Dakshesh <nattamai28@gmail.com>
2025-11-24 16:32:17 +0800

    Added database and seed data.

---
+++ CarRentalManagement/Domain/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRentalManagement.Domain
+{
+    public class Customer : BaseDomainModel
+    {
+        public string? DrivingLicense { get; set; }
+
+        public string? address { get; set; }
+
+        public string? ContactNumber { get; set; }
+
+        public string? EmailAddress { get; set; }
+    }
+}
---
+++ CarRentalManagement/Migrations/20251124082346_SeedData.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarRentalManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Colour",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, "System", new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(8097), new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(8126), "Black", "System" },
+                    { 2, "System", new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(8133), new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(8134), "Blue", "System" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Make",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, "System", new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9558), new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9570), "BMW", "System" },
+                    { 2, "System", new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9577), new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9578), "Toyota", "System" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Model",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateUpdated", "Name", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, "System", new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9822), new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9824), "i4", "System" },
+                    { 2, "System", new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9828), new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9829), "X5", "System" },
+                    { 3, "System", new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9833), new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9834), "Prius", "System" },
+                    { 4, "System", new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9837), new DateTime(2025, 11, 24, 16, 23, 44, 61, DateTimeKind.Local).AddTicks(9838), "C-HR", "System" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
